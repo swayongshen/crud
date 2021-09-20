87ae9a45c6e46d300ff51f711727679b8566b505
Swa Yong Shen <yongshenswa@gmail.com>
2021-09-20 13:45:10 +0800

    Purposely fail test to check if Github detects it

--- spec/requests/put_quotes_spec.rb
+++ spec/requests/put_quotes_spec.rb
@@ -9,7 +9,7 @@ describe "PUT /quotes/:id" do
     @new_quote_title = Faker::Lorem.sentence
     put "/quotes/#{@quote.id}", params: { quote: { title: @new_quote_title} }
 
-    expect(response.status).to eq(200)
+    expect(response.status).to eq(205)
     expect(Quote.find(@quote.id).title).to eq(@new_quote_title)
   end
 end
